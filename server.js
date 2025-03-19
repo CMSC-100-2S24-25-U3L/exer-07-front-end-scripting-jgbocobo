6804e7bb08d9eb1397e63d72dee689fb023a1873
Jorge Bocobo <159723145+jgbocobo@users.noreply.github.com>
2025-03-19 12:36:56 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use(express.static('static_files'))
 
-app.listen(4000, () => {
-    console.log('Server is running on http://localhost:4000/');
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000/');
 });
